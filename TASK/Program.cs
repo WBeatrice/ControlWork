cd3979190ae10713803534b6a66408bdce50491c
Beatrice <b.wenedisiuk@gmail.com>
2023-05-06 16:21:22 +0300

    Added a TASK file with the solution by task

---
+++ TASK/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] CreateArray(int length)
+{
+    string[] array = new string[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Enter an array element: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine();
+}
+
+string[] NewArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int temp = 0;
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[temp] = array[i];
+            temp++;
+        }
+    }
+    return newArray;
+}
+
+
+Console.Write("Enter the length of the array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(length);
+Console.WriteLine();
+Console.WriteLine("Initial array:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("New array:");
+string[] newArray = NewArray(array);
+PrintArray(newArray);
